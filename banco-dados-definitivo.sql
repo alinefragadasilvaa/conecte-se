d80051e2c92734d484830464d9557aba8ba910f7
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2022-08-23 17:40:35 -0300

    Update banco-dados-definitivo.sql

--- banco-dados-definitivo.sql
+++ banco-dados-definitivo.sql
@@ -111,19 +111,27 @@ insert into usuarios_da_plataforma (apelido, cpf, nome, sobrenome, papel, email,
 ("joao", "00000000005", "João", "Trindade", (select papel_id from papeis_usuario where nome="Desenvolvedor"), "10160104@restinga.ifrs.edu.br", "51963215479", "joaosenha1");
 
 insert into categorias_comunidades (nome, descricao, criador) values
-("Finanças", "Gestão das receitas da empresa", (select apelido from usuarios_da_plataforma where nome="Aline"),
-("Marketing", "Estudo do mercado, envolvendo a promoção e propaganda da empresa", (select apelido from usuarios_da_plataforma where nome="Aline"),
-("Gestão de Pessoas", "Organização dos clientes, funcionários, fornecedores e colaboradores da empresa", (select apelido from usuarios_da_plataforma where nome="Aline"),
-("Produção/Operação", "Organização dos processos da empresa", (select apelido from usuarios_da_plataforma where nome="Aline");
+("Finanças", "Gestão das receitas da empresa", (select apelido from usuarios_da_plataforma where nome="Aline")),
+("Marketing", "Estudo do mercado, envolvendo a promoção e propaganda da empresa", (select apelido from usuarios_da_plataforma where nome="Aline")),
+("Gestão de Pessoas", "Organização dos clientes, funcionários, fornecedores e colaboradores da empresa", (select apelido from usuarios_da_plataforma where nome="Aline")),
+("Produção/Operação", "Organização dos processos da empresa", (select apelido from usuarios_da_plataforma where nome="Aline"));
  
- insert into comunidades(nome, descricao, categoria, data_criacao, criador, estado)values
-('Agricultores da Zona Sul - Porto Alegre', 'Comunidade voltada para a divulgação de produtos orgânicos e troca de conhecimentos.', (select categoria_id from categorias_comunidades where nome="Marketing"), '2022/08/23', (select apelido from usuarios_da_plataforma where nome = 'Aline'), "1"),
-('Empreendendo com eficiência','Aprenda a otimizar suas propagandas e obter maiores recompensas.', (select categoria_id from categorias_comunidades where nome="Marketing"),'2022/01/01', (select apelido from usuarios_da_plataforma where nome = 'Bruno'), "1" ),
-('Criatividade - Como maximizar a sua', 'Em um empreendimento a inovação é crucial.', (select categoria_id from categorias_comunidades where nome="Produção/Processos"), '2022/03/09', (select apelido from usuarios_da_plataforma where nome = Bárbara), "1");
+insert into comunidades(nome, descricao, categoria, data_criacao, criador, estado)values
+('Agricultores da Zona Sul - Porto Alegre', 'Comunidade voltada para a divulgação de produtos orgânicos e troca de conhecimentos.', (select categoria_id from categorias_comunidades where nome="Marketing"), '2022-08-23', (select apelido from usuarios_da_plataforma where nome = 'Aline'), "1"),
+('Empreendendo com eficiência','Aprenda a otimizar suas propagandas e obter maiores recompensas.', (select categoria_id from categorias_comunidades where nome="Marketing"),'2022-01-01', (select apelido from usuarios_da_plataforma where nome ='Bruno'), "1" ),
+('Criatividade - Como maximizar a sua', 'Em um empreendimento a inovação é crucial.', (select categoria_id from categorias_comunidades where nome="Produção/Operação"), '2022-03-09', (select apelido from usuarios_da_plataforma where nome ="Bárbara"), "1");
+
+insert into foruns(titulo, assunto, data_criacao, autor, comunidade, estado) values
+('Aumento de vendas','Estou com acúmulo de estoque', "2022-08-23", (select apelido from usuarios_da_plataforma where nome='Aline'),(select comunidade_id from comunidades where nome="Empreendendo com eficiência"), "1"),
+('Plantio de cenoura','Minha mãe pediu para ajuda-la com o plantio de cenoura, porém eu não sei fazer.',"2022-08-23", (select apelido from usuarios_da_plataforma where nome='Bruno'),(select comunidade_id from comunidades where nome="Agricultores da Zona Sul - Porto Alegre"), "1");
+ 
+ insert into respostas_foruns (mensagem, data_criacao, autor, forum) values
+("Monte um sistema de descontos e divulgue para toda sua lista de contatos de clientes, fiz isso mês passado em minha empresa", "2022-08-23", (select apelido from usuarios_da_plataforma where nome="Aline"), (select forum_id from foruns where titulo="Aumento de vendas")),
+("Basta plantá-la em um ambiente abaixo de 25ºC, para isso indico a utilização de ventiladores", "2022-08-23", (select apelido from usuarios_da_plataforma where nome="Bruno"), (select forum_id from foruns where titulo="Plantio de cenoura"));
 
 select * from papeis_usuario;
 select * from usuarios_da_plataforma;
 select * from categorias_comunidades;
 select * from comunidades;
-
-
+select * from foruns;
+ select * from respostas_foruns;
